d106316b39b0c1d1c94c80eb25e3df625b470b79
jellyyams <53629007+jellyyams@users.noreply.github.com>
2025-08-11 17:10:06 +0300

    fix styling for collapsable cards

--- src/data/eia_cards/stages_cards.js
+++ src/data/eia_cards/stages_cards.js
@@ -1,8 +1,9 @@
 const stage_cards = [
   {
-    title: "1. Initial Ethics Assessments → Scoping ethical impact",
+    title: "1. Initial Ethics Assessments",
     text: (
       <>
+        <h3> Inital Ethics Assessments → Scoping ethical impact</h3>
         <p>
           The EIA process begins after projects are conditionally selected
           through the open call process, pending final validation of their
@@ -18,24 +19,35 @@ const stage_cards = [
         <p>This initial assessment includes:</p>
         <ul>
           <li>
-            An overview of the project’s key elements from the perspective of
-            human-centric and ethical technology development.
+            <p>
+              An overview of the project’s key elements from the perspective of
+              human-centric and ethical technology development.
+            </p>
           </li>
           <li>
-            An evaluation of the project’s compliance with Horizon Europe ethics
-            standards and the Ethics Appraisal rules established by the European
-            Commission for all Horizon Europe calls and programmes.
+            <p>
+              An evaluation of the project’s compliance with Horizon Europe
+              ethics standards and the Ethics Appraisal rules established by the
+              European Commission for all Horizon Europe calls and programmes.
+            </p>
           </li>
           <li>
-            An identification of ethical concerns that may arise during any
-            stage of the project’s development.
+            <p>
+              An identification of ethical concerns that may arise during any
+              stage of the project’s development.
+            </p>
           </li>
           <li>
-            A list of recommended actions to address the identified ethical
-            concerns.
+            <p>
+              A list of recommended actions to address the identified ethical
+              concerns.
+            </p>
           </li>
           <li>
-            An assessment of key strengths in the project’s ethics management.
+            <p>
+              {" "}
+              An assessment of key strengths in the project’s ethics management.
+            </p>
           </li>
         </ul>
         <p>
@@ -58,10 +70,12 @@ const stage_cards = [
     ),
   },
   {
-    title:
-      "2. Ethics mentoring, Stage I → Launching ethics management support ",
+    title: "2. Ethics mentoring, Stage I",
     text: (
       <>
+        <h3>
+          Ethics mentoring, Stage I → Launching ethics management support{" "}
+        </h3>
         <p>
           Following the Initial Ethics Assessments, the SSH & Ethics support
           service officially starts, as part of the ARISE mentoring programme.
@@ -79,35 +93,46 @@ const stage_cards = [
         </p>
         <ul>
           <li>
-            SSH & Ethics introductory clinic: Introduce participants to the
-            rationale behind SSH and Ethics aspects and to the process and
-            methods that will be available as support throughout the project.{" "}
+            <p>
+              SSH & Ethics introductory clinic: Introduce participants to the
+              rationale behind SSH and Ethics aspects and to the process and
+              methods that will be available as support throughout the project.{" "}
+            </p>
           </li>
           <li>
-            Workshop – Stakeholder engagement, participation and co-creation:
-            Promotes good practices for identifying and engaging key
-            stakeholders—such as experiment participants, intended users,
-            affected communities, labour unions, partners, or policymakers—and
-            supports the recognition of their needs and preferences in context.
-            The workshop will also introduce practical tools and methods to
-            facilitate meaningful engagement and co-creation.
+            <p>
+              Workshop – Stakeholder engagement, participation and co-creation:
+              Promotes good practices for identifying and engaging key
+              stakeholders—such as experiment participants, intended users,
+              affected communities, labour unions, partners, or policymakers—and
+              supports the recognition of their needs and preferences in
+              context. The workshop will also introduce practical tools and
+              methods to facilitate meaningful engagement and co-creation.
+            </p>
           </li>
-          <li>Survey – Viewpoints toward ethical practices in HRI:</li>
           <li>
-            SSH & Ethics mentoring: The Ethics mentors offer additional support
-            for managing project ethics, providing specific guidance,
-            recommendations and answering questions. Individual sessions can be
-            booked upon request by contacting the mentors directly
+            <p>Survey – Viewpoints toward ethical practices in HRI:</p>
+          </li>
+          <li>
+            <p>
+              SSH & Ethics mentoring: The Ethics mentors offer additional
+              support for managing project ethics, providing specific guidance,
+              recommendations and answering questions. Individual sessions can
+              be booked upon request by contacting the mentors directly
+            </p>
           </li>
         </ul>
       </>
     ),
   },
   {
-    title:
-      "3. Ethics Action Plan → A live document aligning commitments and implementation ",
+    title: "3. Ethics Action Plan",
     text: (
       <>
+        <h3>
+          Ethics Action Plan → A live document aligning commitments and
+          implementation
+        </h3>
         <p>
           Beneficiary projects are required to develop an ethics-specific plan
           to guide their ethics management throughout their involvement in ARISE
@@ -117,35 +142,45 @@ const stage_cards = [
         </p>
         <ul>
           <li>
-            Commitment to Excellence: This section highlights the importance of
-            establishing a strong ethical foundation and clearly communicating
-            this commitment both within the project team and externally. Each
-            project is expected to articulate and demonstrate its dedication to
-            the responsible development of HRI by affirming a human-centric
-            approach, reflecting on their societal and environmental impacts,
-            and adhering with the ARISE ethical principles.
+            <p>
+              {" "}
+              Commitment to Excellence: This section highlights the importance
+              of establishing a strong ethical foundation and clearly
+              communicating this commitment both within the project team and
+              externally. Each project is expected to articulate and demonstrate
+              its dedication to the responsible development of HRI by affirming
+              a human-centric approach, reflecting on their societal and
+              environmental impacts, and adhering with the ARISE ethical
+              principles.
+            </p>
           </li>
           <li>
-            Stakeholder Engagement Plan: This section outlines strategies for
-            engaging stakeholders. Projects should detail a methodology for
-            understanding and tracking stakeholder needs and values, create a
-            stakeholder map, and clearly describe participatory practices.
+            <p>
+              Stakeholder Engagement Plan: This section outlines strategies for
+              engaging stakeholders. Projects should detail a methodology for
+              understanding and tracking stakeholder needs and values, create a
+              stakeholder map, and clearly describe participatory practices.
+            </p>
           </li>
           <li>
-            Compliance to Legal and Ethics Guidelines: This section supports
-            projects in adhering to the latest legal requirements, ethical
-            guidelines, and industry standards. Each project is guided to
-            demonstrate compliance with the EU AI Act and GDPR, complete the
-            ALTAI assessment, and identify other pertinent regulations and
-            guidelines specific to their case.
+            <p>
+              Compliance to Legal and Ethics Guidelines: This section supports
+              projects in adhering to the latest legal requirements, ethical
+              guidelines, and industry standards. Each project is guided to
+              demonstrate compliance with the EU AI Act and GDPR, complete the
+              ALTAI assessment, and identify other pertinent regulations and
+              guidelines specific to their case.
+            </p>
           </li>
           <li>
-            Addressing concerns and recommendations: This section guides
-            projects in compiling and responding to specific ethical concerns
-            and recommendations. These may originate from the ARISE individual
-            ethics assessments or other assessment tools like ALTAI. Projects
-            must detail each concern and the specific actions undertaken or
-            planned to address it.
+            <p>
+              Addressing concerns and recommendations: This section guides
+              projects in compiling and responding to specific ethical concerns
+              and recommendations. These may originate from the ARISE individual
+              ethics assessments or other assessment tools like ALTAI. Projects
+              must detail each concern and the specific actions undertaken or
+              planned to address it.
+            </p>
           </li>
         </ul>
         <p>
@@ -158,10 +193,13 @@ const stage_cards = [
     ),
   },
   {
-    title:
-      "4. Interim Ethics Assessment → Following up on progress and evolving ethical challenges ",
+    title: "4. Interim Ethics Assessment",
     text: (
       <>
+        <h3>
+          Interim Ethics Assessment → Following up on progress and evolving
+          ethical challenges{" "}
+        </h3>
         <p>
           The interim review is conducted by the external ARISE Ethics Committee
           and coordinated by the Ethics mentors. This second ethics assessment
@@ -182,10 +220,13 @@ const stage_cards = [
     ),
   },
   {
-    title:
-      "5.Ethics mentoring, Stage II → Deepening collective understanding of ethics in HRI",
+    title: "5.Ethics mentoring, Stage II",
     text: (
       <>
+        <h3>
+          Ethics mentoring, Stage II → Deepening collective understanding of
+          ethics in HRI
+        </h3>
         <p>
           The Ethics mentoring activities continue after the Interim Ethics
           Assessment. The Ethics mentoring, Stage II contemplates the following
@@ -202,9 +243,10 @@ const stage_cards = [
     ),
   },
   {
-    title: "6. Roadmap for future use → Supporting preparedness for higher TRL",
+    title: "6. Roadmap for future use",
     text: (
       <>
+        <h3>Roadmap for future use → Supporting preparedness for higher TRL</h3>
         <p>
           By the end of the Deployment and Experimentation stage, ARISE Ethics
           Mentors will support each project in drafting an Ethics Roadmap for
@@ -219,9 +261,10 @@ const stage_cards = [
     ),
   },
   {
-    title: "7. Final Ethics Assessment → Evaluating the final outcomes",
+    title: "7. Final Ethics Assessment",
     text: (
       <>
+        <h3> Final Ethics Assessment → Evaluating the final outcomes</h3>
         <p>
           The final review by the external ARISE Ethics Committee will assess
           how and whether or not the set concerns and requirements were achieved
