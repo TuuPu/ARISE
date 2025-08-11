85f7479c84e201fe1b52419411535aeed2911253
jellyyams <53629007+jellyyams@users.noreply.github.com>
2025-08-11 12:55:03 +0300

    add footer requirements

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,8 +7,18 @@ const Footer = () => {
 
   return (
       <footer className="footer">
-        <a href="https://demoshelsinki.fi" target="_blank" rel="noopener noreferrer">Demos Helsinki</a>
-        <p>ARISE project funded by EC (2024-2027)</p>
+        <div className="attribution"> 
+          <img className="flag" alt="EU flag" src="Eu_flag.jpg"></img>
+          <p>Funded by the European Union</p>
+        </div>
+        <div>
+      
+          <a href="https://demoshelsinki.fi" target="_blank" rel="noopener noreferrer">
+          <img className="demoslogo" alt="demos helsinki logo" src="demos_logo.png"></img>
+          </a>
+              <p>Built by Demos Helsinki</p>
+
+        </div>
       </footer>
   
   );
