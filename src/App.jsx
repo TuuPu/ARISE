3f544c0b0d28856e009bebdc22c3fc54b72c7509
jellyyams <53629007+jellyyams@users.noreply.github.com>
2026-01-15 14:30:13 +0200

    add hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import ToolboxPage from "./pages/ToolboxPage.jsx";
 import EthicsAssessmentPage from "./pages/EthicsAssessmentPage";
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,10 +13,10 @@ const Header = () => {
   
       <nav className="global-nav">
         <Link to="/" className={location.pathname === "/" ? "active" : ""}>Home</Link>
-        <Link to="/toolbox" className={location.pathname === "/toolbox" ? "active" : ""}>SSH Toolbox</Link>
-        <Link to="/ethics_assessment" className={location.pathname === "/ethics_assessment" ? "active" : ""}>Ethical Impact Assessment</Link>
+        <Link to="/toolbox/" className={location.pathname === "/toolbox" ? "active" : ""}>SSH Toolbox</Link>
+        <Link to="/ethics_assessment/" className={location.pathname === "/ethics_assessment" ? "active" : ""}>Ethical Impact Assessment</Link>
         <a href="https://arise-middleware.eu" target="_blank" rel="noopener noreferrer">Project Website</a>
-        <Link to="/contact" className={location.pathname === "/contact" ? "active" : ""}>Contact</Link>
+        <Link to="/contact/" className={location.pathname === "/contact" ? "active" : ""}>Contact</Link>
       </nav>
     </header>
   );
